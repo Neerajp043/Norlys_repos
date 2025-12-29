4e357a33344db91f1ba367558999312b657497f6
Neeraj Pratap (EXT-Nokia) <neeraj.pratap.ext@nokia.com>
2025-12-29 13:15:14 +0530

    added jackie

--- main.tf
+++ main.tf
@@ -7,3 +7,7 @@ resource "azurerm_resource_group" "rg_neeraj29" {
   name     = "rg_neeraj29"
   location = "Westus"
 }
+resource "azurerm_resource_group" "rg_jackie" {
+  name     = "rg_jackie"
+  location = "Westus"
+}
