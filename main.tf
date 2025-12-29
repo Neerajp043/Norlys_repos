df935ebf1be0672aaef0e94c818ae5b04f6a8fa9
Neeraj Pratap (EXT-Nokia) <neeraj.pratap.ext@nokia.com>
2025-12-29 13:10:15 +0530

    added jack

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg_neeraj1" {
   name     = "rg_neeraj1"
   location = "Westus"
 }
+
+resource "azurerm_resource_group" "rg_neeraj29" {
+  name     = "rg_neeraj29"
+  location = "Westus"
+}
